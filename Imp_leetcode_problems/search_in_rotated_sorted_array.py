20aee8a47a7df9a8b0d7e54b371471b536df9269
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2025-12-23 00:14:45 +0530

    file

---
+++ Imp_leetcode_problems/search_in_rotated_sorted_array.py
@@ -0,0 +1,35 @@
+# Example 1:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+# Example 2:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+# Example 3:
+
+# Input: nums = [1], target = 0
+# Output: -1
+
+class Solution:
+    def search(self, nums, target: int) -> int:
+        left, right = 0, len(nums) - 1
+    
+        while left <= right:
+            mid = left + (right - left) // 2
+            
+            if nums[mid] == target:
+                return mid
+        
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
+        return -1

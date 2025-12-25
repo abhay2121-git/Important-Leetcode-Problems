d46183a601c79a5d3268ae25e42aeb3f77c728f3
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2025-12-25 21:20:02 +0530

    file

---
+++ Imp_leetcode_problems/Remove_Element.py
@@ -0,0 +1,18 @@
+# Example 1:
+# Input: nums = [3,2,2,3], val = 3
+# Output: 2, nums = [2,2,_,_]
+
+# Example 2:
+# Input: nums = [0,1,2,2,3,0,4,2], val = 2
+# Output: 5, nums = [0,1,4,0,3,_,_,_]
+
+class Solution:
+    def removeElement(self, nums, val: int) -> int:
+        k = 0
+
+        for i in range(len(nums)):
+            if nums[i] == val:
+                nums[k] = nums[i]
+                k += 1
+            
+        return k

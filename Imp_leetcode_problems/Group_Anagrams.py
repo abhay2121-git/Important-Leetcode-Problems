9dd33a56c051e2dfe25de738838e38393aa2225e
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-01-17 00:04:08 +0530

    file

---
+++ Imp_leetcode_problems/Group_Anagrams.py
@@ -0,0 +1,19 @@
+# Example 1:
+
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+from collections import defaultdict
+from typing import List
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams_dict = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            key = tuple(count)
+            anagrams_dict[key].append(s)
+ 
+        return list(anagrams_dict.values())

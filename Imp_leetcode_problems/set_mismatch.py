7b9963583fcab36347ea0fb2735e055e9ea4d717
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-01-21 13:16:45 +0530

    file

---
+++ Imp_leetcode_problems/set_mismatch.py
@@ -0,0 +1,20 @@
+# Example 1:
+# Input: nums = [1,2,2,4]
+# Output: [2,3]
+
+# Example 2:
+# Input: nums = [1,1]
+# Output: [1,2]
+
+from typing import List
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+        unique_sum = sum(set(nums))
+
+        duplicate = actual_sum - unique_sum
+        missing = expected_sum - unique_sum
+
+        return [duplicate, missing]

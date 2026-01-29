508c82317cd771d57be866e4d4cc9743da2217f3
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-01-29 17:56:04 +0530

    file

---
+++ Imp_leetcode_problems/baseball_game.py
@@ -0,0 +1,23 @@
+# Example 1:
+# Input: ops = ["5","2","C","D","+"]
+# Output: 30
+
+# Example 2:
+# Input: ops = ["5","-2","4","C","D","9","+","+"]
+# Output: 27
+
+from typing import List
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        for i in operations:
+            if i == 'D':
+                stack.append(2 * stack[-1])
+            elif i == 'C':
+                stack.pop()
+            elif i == '+':
+                stack.append(stack[-1] + stack[-2])
+            else:
+                stack.append(int(i))
+
+        return sum(stack)

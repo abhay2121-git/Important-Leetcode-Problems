37b9a82b07fb5bd051a912ba4863c1d3bae0dfa3
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-01-30 20:51:00 +0530

    file

---
+++ Imp_leetcode_problems/split_the_array.py
@@ -0,0 +1,18 @@
+# Example 1:
+# Input: nums = [1,1,2,2,3,4]
+# Output: true
+
+# Example 2:
+# Input: nums = [1,1,1,1]
+# Output: false
+
+from collections import Counter
+from typing import List
+class Solution:
+    def isPossibleToSplit(self, nums: List[int]) -> bool:
+        counter = Counter(nums)
+
+        for f in counter.values():
+            if f > 2:
+                return False
+        return True

4089ac30808c067fc7be92f00dd5068cc17a128a
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-01-31 22:50:31 +0530

    file

---
+++ Imp_leetcode_problems/find_smallest_letter_greater_than_target.py
@@ -0,0 +1,34 @@
+# Example 1:
+# Input: letters = ["c","f","j"], target = "a"
+# Output: "c"
+
+# Example 2:
+# Input: letters = ["c","f","j"], target = "c"
+# Output: "f"
+
+# Example 3:
+# Input: letters = ["x","x","y","y"], target = "z"
+# Output: "x"
+
+from typing import List
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        l = 0
+        r = len(letters) - 1
+
+        if target >= letters[-1]:
+            return letters[0]
+        
+        while l < r:
+            mid = l + (r - l) // 2
+
+            if letters[mid] > target:
+                r = mid
+            else:
+                l = mid + 1
+        
+        return letters[l]
+
+letters = ["c","f","j"]
+target = "a"
+print(Solution().nextGreatestLetter(letters, target))

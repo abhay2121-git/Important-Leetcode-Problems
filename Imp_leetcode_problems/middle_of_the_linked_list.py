8fa141e68f51c3454b499ec2d45a9666334bf8ed
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-02-05 19:58:13 +0530

    file

---
+++ Imp_leetcode_problems/middle_of_the_linked_list.py
@@ -0,0 +1,51 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        return slow
+
+
+def create_linked_list(arr):
+    if not arr:
+        return None
+    
+    head = ListNode(arr[0])
+    current = head
+    
+    for val in arr[1:]:
+        current.next = ListNode(val)
+        current = current.next
+    
+    return head
+
+
+def print_linked_list(head):
+    result = []
+    current = head
+    
+    while current:
+        result.append(str(current.val))
+        current = current.next
+    
+    print("[" + ",".join(result) + "]")
+
+
+# ✅ Proper execution
+arr = [1, 2, 3, 4, 5]
+head = create_linked_list(arr)
+
+middle = Solution().middleNode(head)
+print_linked_list(middle)

ad10c523022fe5f05efc0b9974597abad3178802
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-02-12 19:18:33 +0530

    file

---
+++ Imp_leetcode_problems/intersection_of_two_arrays.py
@@ -0,0 +1,29 @@
+# Example 1:
+# Input: nums1 = [1,2,2,1], nums2 = [2,2]
+# Output: [2]
+
+# Example 2:
+# Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+# Output: [9,4]
+
+from typing import List
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+
+        i = j = 0
+        result = []
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                if not result or result[-1] != nums1[i]:
+                    result.append(nums1[i])
+                
+                i += 1
+                j += 1
+        return result

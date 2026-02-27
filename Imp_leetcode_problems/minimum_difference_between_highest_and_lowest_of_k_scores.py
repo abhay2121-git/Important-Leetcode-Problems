8f72dd31a2e0e7a5aa4e29204e79f9ee7e0f4d4e
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-02-27 18:05:46 +0530

    file

---
+++ Imp_leetcode_problems/minimum_difference_between_highest_and_lowest_of_k_scores.py
@@ -0,0 +1,26 @@
+# minimum_difference_between_highest_and_lowest_of_k_scores
+
+# Example 1:
+# Input: nums = [90], k = 1
+# Output: 0
+
+# Example 2:
+# Input: nums = [9,4,1,7], k = 2
+# Output: 2
+
+from typing import List
+class Solution:
+    def minimumDifference(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        min_diff = float('inf')
+        n = len(nums)
+
+        for i in range(n - k + 1):
+            diff = nums[i + k - 1] - nums[i]
+            min_diff = min(min_diff, diff)
+
+        return min_diff
+
+nums = [9, 4, 1, 7]
+k = 2
+print(Solution().minimumDifference(nums, k))

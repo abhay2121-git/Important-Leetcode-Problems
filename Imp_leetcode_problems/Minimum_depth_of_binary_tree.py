01fa623a541a3761a442972b93d116bba3ba51f5
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-03-08 16:16:01 +0530

    file

---
+++ Imp_leetcode_problems/Minimum_depth_of_binary_tree.py
@@ -0,0 +1,38 @@
+# Example 1:
+# Input: root = [3,9,20,null,null,15,7]
+# Output: 2
+
+# Example 2:
+# Input: root = [2,null,3,null,4,null,5,null,6]
+# Output: 5
+
+from collections import deque
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        queue = deque([(root, 1)])
+
+        while queue:
+            node, depth = queue.popleft()
+
+            if not node.left and not node.right:
+                return depth
+            
+            if node.left:
+                queue.append((node.left, depth + 1))
+            
+            if node.right:
+                queue.append((node.right, depth + 1))
+
+        return 0
+    

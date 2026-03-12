8066a1c1a36395739602c165048bfa4fb0391e10
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-03-12 16:36:19 +0530

    file

---
+++ Imp_leetcode_problems/even_odd_linked_list.py
@@ -0,0 +1,29 @@
+# Example 1:
+# Input: head = [1,2,3,4,5]
+# Output: [1,3,5,2,4]
+
+# Example 2:
+# Input: head = [2,1,3,5,6,4,7]
+# Output: [2,3,6,7,1,5,4]
+
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        odd = head
+        even = head.next
+        even_head = head.next
+
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+
+        odd.next = even_head
+    
+        return head

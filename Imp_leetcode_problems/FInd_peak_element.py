f09f4f355361aa6ef35f44579c3d8403e7a2300c
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-03-13 15:43:09 +0530

    file

---
+++ Imp_leetcode_problems/FInd_peak_element.py
@@ -0,0 +1,23 @@
+# Example 1:
+# Input: nums = [1,2,3,1]
+# Output: 2
+
+# Example 2:
+# Input: nums = [1,2,1,3,5,6,4]
+# Output: 5
+
+from typing import List
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        low, high = 0, len(nums) - 1
+
+        while low < high:
+            mid = (low + high) >> 1
+
+            if nums[mid] < nums[mid + 1]:
+                low = mid + 1
+            else:
+                high = mid
+        
+        return low
+    

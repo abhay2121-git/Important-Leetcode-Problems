e56ddda954c7691e4f41f6d5ae69e4a151c2eb78
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-03-13 15:49:02 +0530

    file updated

--- Imp_leetcode_problems/FInd_peak_element.py
+++ Imp_leetcode_problems/FInd_peak_element.py
@@ -6,6 +6,7 @@
 # Input: nums = [1,2,1,3,5,6,4]
 # Output: 5
 
+# "Binary Search on Condition"
 from typing import List
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
@@ -20,5 +21,4 @@ class Solution:
                 high = mid
         
         return low
-    
     

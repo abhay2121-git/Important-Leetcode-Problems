ff1755661e07405af872a04b57224c6fe3c5bd42
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-03-17 19:54:56 +0530

    file

---
+++ Imp_leetcode_problems/Symmetric_Tree.py
@@ -0,0 +1,28 @@
+# Example 1:
+# Input: root = [1,2,2,3,4,4,3]
+# Output: true
+
+# Example 2:
+# Input: root = [1,2,2,null,3,null,3]
+# Output: false
+
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        
+        def isMirror(left, right):
+            if not left and not right:
+                return True
+            
+            if not left or not right:
+                return False
+
+            return (left.val == right.val) and isMirror(left.left, right.right) and isMirror(left.right, right.left)
+
+        return isMirror(root.left, root.right)

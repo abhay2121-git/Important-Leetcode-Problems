31473c4b5bee2495b56938f39a7f7ffe074a699a
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-09-08 22:54:05 +0530

    Update Jump_Game_II.py

--- Imp_leetcode_problems/Jump_Game_II.py
+++ Imp_leetcode_problems/Jump_Game_II.py
@@ -17,14 +17,14 @@ class Solution:
         curr_end = 0
         farthest = 0
 
-        for i in range(n):
+        for i in range(n - 1): # for i in range(n):
             farthest = max(farthest, i + nums[i])
 
             if i == curr_end:
                 jumps += 1
                 curr_end = farthest
 
-            if curr_end == n - 1:
-                break
+            # if curr_end == n - 1:
+            #     break
         
         return jumps

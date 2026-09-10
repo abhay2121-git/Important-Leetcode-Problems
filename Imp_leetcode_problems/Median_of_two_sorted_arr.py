62e8ca6680c4d498a6667b4417cf1961fe5215a9
Abhay Bhanarkar <abhaybhanarkar2005@gmail.com>
2026-09-10 17:20:36 +0530

    Fix Median of two sorted array

--- Imp_leetcode_problems/Median_of_two_sorted_arr.py
+++ Imp_leetcode_problems/Median_of_two_sorted_arr.py
@@ -1,10 +1,6 @@
 # Median of a sorted array
 class Median:
-    def __init__(self, nums1, nums2):
-        self.nums1 = nums1
-        self.nums2 = nums2
-    
-    def median(self):
+    def median(self, nums1, nums2):
         nums = nums1 + nums2
         nums = sorted(nums)
         mid = len(nums) // 2
@@ -24,23 +20,3 @@ print(numss)
 # nums1 = [1, 2, 3, 4]
 # nums2 = [5, 6]
 # Output : 3.5
-
-
-class Solution:
-    def __init__(self):
-        pass
-
-    def median_of_arrs(self, nums1, nums2):
-        nums = nums1 + nums2
-        nums = sorted(nums)
-        mid = len(nums) // 2
-
-        if len(nums) %2 != 0:
-            return nums[mid]
-        else:
-            mid = nums[mid - 1] + nums[mid]
-            return mid/2.0
-
-nums1 = [1, 2, 3]
-nums2 = [4]
-print(Solution().median_of_arrs(nums1, nums2))

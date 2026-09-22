390e6fe86dbf30587044613a765d2f7f4b03ceea
abhay2121-git <abhaybhanarkar2005@gmail.com>
2026-09-22 20:38:02 +0530

    Update file

--- Imp_leetcode_problems/Median_of_two_sorted_arr.py
+++ Imp_leetcode_problems/Median_of_two_sorted_arr.py
@@ -1,4 +1,9 @@
 # Median of a sorted array
+
+# nums1 = [1, 2, 3, 4]
+# nums2 = [5, 6]
+# Output : 3.5
+
 class Median:
     def median(self, nums1, nums2):
         nums = nums1 + nums2
@@ -16,7 +21,3 @@ nums2 = [5, 6]
 numss = Median(nums1, nums2).median()
 print(numss)
 
-
-# nums1 = [1, 2, 3, 4]
-# nums2 = [5, 6]
-# Output : 3.5
